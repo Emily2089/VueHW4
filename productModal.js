a18be03e574c6de864f4484907c328ccc4585f65
Your Name <you@example.com>
2024-02-10 14:55:31 +0800

    將部分 div 改成 template

--- productModal.js
+++ productModal.js
@@ -36,7 +36,7 @@ export default {
             </div>
             <h3>多圖新增</h3>
             <!-- note： Array.isArray() 可用來確認傳入的內容是否為一個陣列-->
-            <div v-if="Array.isArray(tempProduct.demoImg)" class="mb-3">
+            <template v-if="Array.isArray(tempProduct.demoImg)" class="mb-3">
               <div v-for="(image, key) in tempProduct.demoImg" :key="key + 1912" class="mb-3">
                 <div class="mb-3">
                   <label :for="'demoImg'+key" class="form-label">圖片網址</label>
@@ -45,18 +45,18 @@ export default {
                 <img class="img-fluid" :src="image" alt="">
               </div>
               <!-- note：跳出視窗後，因為 tempProduct 的 demoImg 仍是空陣列，所以 !tempProduct.demoImg.length 的結果為 true，因此顯示 "新增圖片" 的按鈕。第一次按下 "新增圖片" 後，會在 demoImg 產生一個第0筆資料其 value 為空字串，等輸入文字後，透過 v-model 將輸入的文字綁定進去，第0筆資料就完成了，有 key 有 value，因此 tempProduct.demoImg[tempProduct.demoImg.length - 1] 就會存在，結果為 true -->
-              <div v-if="!tempProduct.demoImg.length || tempProduct.demoImg[tempProduct.demoImg.length - 1]">
+              <template v-if="!tempProduct.demoImg.length || tempProduct.demoImg[tempProduct.demoImg.length - 1]">
                 <button class="btn btn-outline-primary btn-sm d-block w-100" @click="tempProduct.demoImg.push('')">
                   新增圖片
                 </button>
-              </div>
-              <div v-else>
+              </template>
+              <template v-else>
                 <!-- note：Array.prototype.pop() 會移除並回傳陣列的最後一個元素。此方法會改變陣列的長度。 -->
                 <button class="btn btn-outline-danger btn-sm d-block w-100" @click="tempProduct.demoImg.pop()">
                   刪除圖片
                 </button>
-              </div>
-            </div>
+              </template>
+            </template>
             <div v-else>
               <button class="btn btn-outline-danger btn-sm d-block w-100" @click="tempProduct.demoImg.pop()">
                 刪除圖片
@@ -100,25 +100,25 @@ export default {
             </div>
             <div class="mb-3">
               <h5>遊戲特色</h5>
-              <div v-if="Array.isArray(tempProduct.features)" class="mb-3">
+              <template v-if="Array.isArray(tempProduct.features)" class="mb-3">
                 <div v-for="(feature, key) in tempProduct.features" :key="key + 2841" class="mb-3">
                   <label :for="'feature'+key" class="form-label">特色 {{ key+1 }}</label>
                   <textarea :id="'feature'+key" type="text" class="form-control"
                     placeholder="請輸入說明內容" v-model="tempProduct.features[key]">
                   </textarea>
                 </div>
-                <div v-if="!tempProduct.features.length || tempProduct.features[tempProduct.features.length -1]">
+                <template v-if="!tempProduct.features.length || tempProduct.features[tempProduct.features.length -1]">
                   <button class="btn btn-outline-primary btn-sm d-block w-100" @click="tempProduct.features.push('')">新增遊戲特色</button>
-                </div>
-                <div v-else>
+                </template>
+                <template v-else>
                   <button class="btn btn-outline-primary btn-sm d-block w-100" @click="tempProduct.features.pop()">刪除遊戲特色</button>
-                </div>
-              </div>
-              <!-- <div v-else>
+                </template>
+              </template>
+              <template v-else>
                 <button class="btn btn-outline-danger btn-sm d-block w-100" @click="tempProduct.features.pop()">
                   刪除遊戲特色
                 </button>
-              </div> -->
+              </template>
             </div>
             <div class="mb-3">
               <div class="form-check">
